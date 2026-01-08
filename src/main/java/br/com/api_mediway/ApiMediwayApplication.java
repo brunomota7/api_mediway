059329956cb54c7df5697484493c53d99b897714
brunomota7 <bruno7motadev@gmail.com>
2026-01-08 14:35:18 -0300

    Iniciando repositorio no GitHub

---
+++ src/main/java/br/com/api_mediway/ApiMediwayApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_mediway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMediwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMediwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api_mediway/ApiMediwayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_mediway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMediwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
